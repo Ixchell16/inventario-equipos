08007a78014cb3c6d2bddabc29dec29f70a2f4ba
Ixchell16 <ixchellsalgado.nazario@utacapulco.edu.mx>
2024-05-22 23:27:12 -0600

    conexion

--- config.js
+++ config.js
@@ -1,11 +1,17 @@
 
 const PORT = process.env.PORT || 3000
 
-const DB_HOST = process.env.DB_HOST || 'localhost'
+/* const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'ixchell12345'
 const DB_NAME = process.env.DB_NAME || 'telmex'
-const DB_PORT = process.env.DB_PORT || 3306
+const DB_PORT = process.env.DB_PORT || 3306 */
+
+const DB_HOST = process.env.DB_HOST || 'viaduct.proxy.rlwy.net'
+const DB_USER = process.env.DB_USER || 'root'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'LDLiMOsexvWYAXOfomuLhCBTDCwjfJVO'
+const DB_NAME = process.env.DB_NAME || 'railway'
+const DB_PORT = process.env.DB_PORT || 19426
 
 module.exports = {
     PORT,
